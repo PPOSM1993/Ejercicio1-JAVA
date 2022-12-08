9cecd49bd55124fc0018299fa78b2c7104e34673
pedroosorio <pedro.osorio04@inacapmail.cl>
2022-12-07 23:08:29 -0300

    Initial commit

---
+++ src/clase/Ejercicio1.java
@@ -0,0 +1,27 @@
+package clase;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        //Definimos las variables.
+        String saludo = "Hola Mundo";
+        int numero = 1;
+        float floante = 1.6F;
+        double doble = 5.5;
+        boolean mayor = true;
+        short diasLustro = (12 * 31) * 5;
+        char caracter = 'S';
+        byte numero2 = 1;
+
+        //Resultados que imprimen los datos por consola en JAVA.
+        System.out.println(saludo);
+        System.out.println(numero);
+        System.out.println(floante);
+        System.out.println(doble);
+        System.out.println(mayor);
+        System.out.println(diasLustro);
+        System.out.println(caracter);
+        System.out.println(numero2);
+
+    }
+}
